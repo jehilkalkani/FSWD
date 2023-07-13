59349c2d4771eb404b39b4a5a6ce6c0636c8174b
Jeh!l k@lk@n! <96511181+jehilkalkani@users.noreply.github.com>
2023-07-13 15:09:17 +0530

    Add files via upload

---
+++ Week-2/ReadEmployee.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+let i =0 ;
+const list = () =>{
+    try{
+    const readData = fs.readFileSync('employee.json');
+    const datajson = JSON.parse(readData)
+    return  datajson 
+    }catch(e){
+        return [];
+    }
+}
+const data = process.argv[2]
+let arr =[]
+arr  = list()
+arr.push({name : data})
+
+fs.writeFileSync('employee.json',
+    JSON.stringify(arr) 
+)
+ arr  = list()
+console.log(arr)
